d682fd093c30fb270f36f309653a67486cab0389
willyc1701 <88260789+willyc1701@users.noreply.github.com>
2023-09-25 16:17:21 +0800

    Add components

---
+++ components/Post.js
@@ -0,0 +1,13 @@
+function Post({ image, name, setSelectedPostName }) {
+    return (
+        <li className='post-component'>
+          <button onClick={() => setSelectedPostName(name)}>
+            <img src={image} alt={name}/>
+            <p className="post-name">{name}</p>
+          </button>
+        </li>
+    )
+  }
+  
+  export default Post;
+  
